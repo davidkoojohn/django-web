3717c4f4b85ee3be84722b934f5cbc6302e32ee5
davidkoojohn <davidkoojohn@gmail.com>
2018-07-25 11:07:13 +0800

    合并路由

--- fuckdjango/urls.py
+++ fuckdjango/urls.py
@@ -1,9 +1,13 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
+from hellodjango import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('hrs/', include('hrs.urls')),
-    path('', include('hellodjango.urls')),
+  path('admin/', admin.site.urls),
+  # path('hrs/', include('hrs.urls')),
+  # path('', include('hellodjango.urls')),
+
+  path('', views.index),
+  path('subject/<int:no>/', views.show)
 ]
