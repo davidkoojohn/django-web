bb72d9eeee1680e4d83eacf80dd28d01e0f4ebf5
davidkoojohn <davidkoojohn@gmail.com>
2018-07-26 18:55:22 +0800

    添加admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+class QuestionAdmin(admin.ModelAdmin):
+  list_display = ('id', 'question_text', 'pub_date')
+  ordering = ('id',)
+
+class ChoiceAdmin(admin.ModelAdmin):
+  list_display = ('question', 'choice_text', 'votes')
+  ordering = ('id',)
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
+
