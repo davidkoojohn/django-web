a2a8860e0a99a502ccf4f8a69f5177bc7bb0162f
davidkoojohn <davidkoojohn@gmail.com>
2018-07-27 17:58:08 +0800

    usage 404

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
 from django.shortcuts import render
 
@@ -18,9 +18,15 @@ def index(request):
 
   return render(request, 'polls/index.html', context)
 
-def detail(request, question_id):
 
-  return HttpResponse('detail %s' % question_id)
+
+def detail(request, question_id):
+  try:
+    question = Question.objects.get(pk=question_id)
+  except Question.DoesNotExist:
+    raise Http404('Question does not exist.')
+
+  return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
